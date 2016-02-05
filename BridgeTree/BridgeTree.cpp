d8c10d7dc55609fbccdf09a8915e01769e242072
Tanuj04 <tanujkhattar4@gmail.com>
2016-02-05 16:13:14 +0530

    Improved indentation issues

--- BridgeTree/BridgeTree.cpp
+++ BridgeTree/BridgeTree.cpp
@@ -5,53 +5,53 @@ int vis[N],arr[N],T,cmpno;//supporting stuff
 queue<int> Q[N];
 
 int adj(int u,int e){
-  return U[e]==u?V[e]:U[e];
+	return U[e]==u?V[e]:U[e];
 }
 
 int dfs0(int u,int edge)//mark bridges
 { 
-  int dbe = arr[u] = T++;
-  vis[u]=1;
-  for(int i=0;i<SZ(graph[u]);i++)
+	int dbe = arr[u] = T++;
+	vis[u]=1;
+	for(int i=0;i<SZ(graph[u]);i++)
 	{
-    int e = graph[u][i];
+		int e = graph[u][i];
 		int w = adj(u,e);
-    if(!vis[w])
+		if(!vis[w])
 			dbe = min(dbe,dfs0(w,e));
-    else if(e!=edge)
+		else if(e!=edge)
 			dbe = min(dbe,arr[w]);
-  }
-  if(dbe == arr[u] && edge!=-1)
+	}
+	if(dbe == arr[u] && edge!=-1)
 		isbridge[edge]=true;
-  return dbe;
+	return dbe;
 }
 
 void dfs1(int v)	//Builds the tree with each edge a bridge from original graph
 {
-  int currcmp = cmpno; // current component no.
-  Q[currcmp].push(v);// A different queue for each component, coz during bfs we shall go to another component (step of dfs) and then come   back to this one and continue our bfs
+	int currcmp = cmpno; // current component no.
+	Q[currcmp].push(v);// A different queue for each component, coz during bfs we shall go to another component (step of dfs) and then come   back to this one and continue our bfs
 	vis[v]=1;
-  while(!Q[currcmp].empty()) //bfs. Exploring all nodes of this  component
+	while(!Q[currcmp].empty()) //bfs. Exploring all nodes of this  component
 	{		
-    int u = Q[currcmp].front();
+		int u = Q[currcmp].front();
 		Q[currcmp].pop();    
-    for(int i=0;i<SZ(graph[u]);i++)
+		for(int i=0;i<SZ(graph[u]);i++)
 		{
-      int e = graph[u][i];
+			int e = graph[u][i];
 			int w = adj(u,e);
-      if(vis[w])continue;
-      if(isbridge[e])		//if the edge under consideration is bridge and other side is not yet explored, go there (step of dfs)
+			if(vis[w])continue;
+			if(isbridge[e])		//if the edge under consideration is bridge and other side is not yet explored, go there (step of dfs)
 			{
 				cmpno++;
-        tree[currcmp].push_back(cmpno);
-        tree[cmpno].push_back(currcmp);
-        dfs1(w);
-      }
-      else	//else continue with our bfs
+				tree[currcmp].push_back(cmpno);
+				tree[cmpno].push_back(currcmp);
+				dfs1(w);
+			}
+			else	//else continue with our bfs
 			{
 				Q[currcmp].push(w);
 				vis[w]=1;
 			}
-    }
-  }
+		}
+	}
 }
